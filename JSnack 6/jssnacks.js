488ef5dfdc37f203fba04ce2ad02e7f559a99149
EzexnDev <gianmarco.cavalieredev@gmail.com>
2020-11-12 17:52:28 +0100

    fine

--- JSnack 6/jssnacks.js
+++ JSnack 6/jssnacks.js
@@ -1,16 +1,30 @@
-var arrayObj = [
-    { name: 'Poppy', type: 'tshirt', color: 'red' },
-    { name: 'Jumping', type: 'occhiali', color: 'blue' },
-    { name: 'CrissCross', type: 'scarpe', color: 'black' },
-    { name: 'Jenny', type: 'borsa', color: 'pink' },
+var arrayObj = [{
+        name: 'Poppy',
+        type: 'tshirt',
+        color: 'red'
+    },
+    {
+        name: 'Jumping',
+        type: 'occhiali',
+        color: 'blue'
+    },
+    {
+        name: 'CrissCross',
+        type: 'scarpe',
+        color: 'black'
+    },
+    {
+        name: 'Jenny',
+        type: 'borsa',
+        color: 'pink'
+    },
 ];
 
 let alphabets = "abcdefghijklmnopqrstuvwxyz";
 
-var newArrayObj = arrayObj.map((element) => {
-    var o = Object.assign({}, element);
-    o.position = 's';
-    return o;
-});
+newArrayObj = newArrayObj.map(el => ({
+    ...el,
+    position: alphabet[Math.floor(Math.random() * alphabet.length)]
+}));
 console.log(newArrayObj);
 console.log(arrayObj);
